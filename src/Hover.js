d0c17dff592be2791888cc422afed1a414634c79
Susanne Lundkvist <susanne.r.lundkvist@gmail.com>
2019-07-09 17:03:07 +0100

    testing branch

--- src/Hover.js
+++ src/Hover.js
@@ -5,6 +5,7 @@ const Hover = ({ onHover, children }) => (
     <div className="hover">
         <div className="hover__no-hover">{children}</div>
         <div className="hover__hover">{onHover}</div>
+        <div>Making some changes in hover to test branch</div>
     </div>
 )
 

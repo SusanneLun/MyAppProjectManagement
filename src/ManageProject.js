b9223c90cc9d26d26a78c7aac0a9ab327a8a275f
Susanne Lundkvist <susanne.r.lundkvist@gmail.com>
2019-09-07 21:04:13 +0100

    experimenting with form input vs textarea in manage project

--- src/ManageProject.js
+++ src/ManageProject.js
@@ -14,6 +14,11 @@ class ManageProject extends Component {
     }
   }
 
+
+    handleChange = (event, value) => {
+    this.setState({ [event.target.name]: event.target.value })
+    }
+
   componentDidMount() {
     const { id } = this.props.match.params
     APILogin.getUserProject(id)
@@ -23,13 +28,31 @@ class ManageProject extends Component {
     }
 
 render() {
+  const { id } = this.props.match.params
   return (
     <div>
-
+    <Form onSubmit={() => this.handleSubmit(id)} style={{marginLeft: 50, top: 80}}>
+    <h3>View And Edit Project</h3>
     <Form.Group widths='equal' >
       <Form.Input  name="name" fluid label="Name" type="text" onChange={event => this.handleChange(event)} value={this.state.name} />
-      <Form.Input name="description" fluid label='Description' type="text" onChange={this.handleChange}  value={this.state.description}/>
+      <Form.Input name="description" fluid label='Description' rows="5" type="text" onChange={this.handleChange}  value={this.state.description}/>
     </Form.Group>
+    <div className="ui form">
+    <div className="field">
+      <label>Text</label>
+      <textarea></textarea>
+    </div>
+    <div className="field">
+      <label>Short Text</label>
+      <textarea rows="2"></textarea>
+    </div>
+    </div>
+
+    <div className={"new_stakeholder_submit"}>
+    <Form.Button type="submit"  color="purple"> Update Project </Form.Button>
+    <Form.Button type="delete"  color="red" onClick={() => this.handleDelete(id)}> Delete Project </Form.Button>
+    </div>
+    </Form>
 
     </div>
   )

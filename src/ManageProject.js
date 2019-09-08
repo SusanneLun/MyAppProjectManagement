978304f5a08569e2c4956f78aed6025ab25ddfd3
Susanne Lundkvist <susanne.r.lundkvist@gmail.com>
2019-09-08 18:13:01 +0100

    textareas replaced form input and backend needs adjusting

--- src/ManageProject.js
+++ src/ManageProject.js
@@ -27,24 +27,48 @@ class ManageProject extends Component {
 
     }
 
+
+
+      handleSubmit = (project, updatedValues) => {
+
+
+        let updatedValue = {
+          name: updatedValues.name === "" ? project.name : updatedValues.name,
+          title: updatedValues.description === "" ? project.description : updatedValues.description,
+          id: this.props.match.params
+      }
+
+        fetch(`http://localhost:3000/user_project/${project.id}`, {
+          method: 'PATCH',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify( updatedValue ),
+        }).then(res => res.json())
+        .then(project => {
+          this.setState({
+            name: project.name,
+            description: project.description,
+            })})
+      }
+
 render() {
   const { id } = this.props.match.params
   return (
     <div>
-    <Form onSubmit={() => this.handleSubmit(id)} style={{marginLeft: 50, top: 80}}>
+    <Form onSubmit={() => this.handleSubmit(id, this.state)} style={{marginLeft: 50, top: 80}}>
     <h3>View And Edit Project</h3>
-    <Form.Group widths='equal' >
-      <Form.Input  name="name" fluid label="Name" type="text" onChange={event => this.handleChange(event)} value={this.state.name} />
-      <Form.Input name="description" fluid label='Description' rows="5" type="text" onChange={this.handleChange}  value={this.state.description}/>
-    </Form.Group>
+
     <div className="ui form">
     <div className="field">
       <label>Text</label>
-      <textarea></textarea>
+      <textarea name="name" fluid label="Name" type="text" onChange={event => this.handleChange(event)} value={this.state.name} >
+      </textarea>
     </div>
     <div className="field">
       <label>Short Text</label>
-      <textarea rows="2"></textarea>
+      <textarea rows="2" name="description" fluid label='Description' type="text" onChange={this.handleChange}  value={this.state.description}>
+      </textarea>
     </div>
     </div>
 

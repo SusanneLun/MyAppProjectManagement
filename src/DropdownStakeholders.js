ac813ea0d11ec53a53ececadeffa4fbd89ab6d4d
Susanne Lundkvist <susanne.r.lundkvist@gmail.com>
2020-03-15 14:35:35 +0000

    npm upgrade

--- src/DropdownStakeholders.js
+++ src/DropdownStakeholders.js
@@ -11,28 +11,30 @@ constructor() {
   }
 }
 
-handleChange = (event, data) => {
-  this.setState({ [data.name]: data.value })
-}
+
+  handleChange = (event, data) => {
+    this.setState({ [data.name]: data.value })
+  }
 
 
 render() {
 
-  const options = this.props.stakeholders.map(stakeholder => {return {name: stakeholder.name}})
+  const options = this.props.stakeholders.map(stakeholder => stakeholder.name)
 
   return(
   <div>
   <Form.Group widths='equal' >
     <div className={"new_stakeholder_input"}>
       <label>Select Stakeholder</label>
-      <Select name="name" label="Name" type="text" options={options}
-          placeholder="Select" value={this.state.name}
-              onChange={this.handleChange}  />
+      <Select name="name" text={options} label="Name" options={options}
+        placeholder="Select" value={this.state.name}
+        onChange={this.handleChange} />
     </div>
   </Form.Group>
-
   </div>
 )}
 }
+
+
 
 export default DropdownStakeholders
